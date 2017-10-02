74e47e6a7be35ec36a0be03311adf026dd632de6
Ryan Harings <ryanharings@hotmail.com>
2017-10-02 17:37:45 -0600

    loaded all data except trougher eff

--- calculations.js
+++ calculations.js
@@ -1,6 +1,3 @@
 function multiply(number) {
   return number * number;
 }
-
-console.log(multiply(3));
-console.log(multiply(5));
---
+++ lumenData.js
@@ -0,0 +1,3 @@
+const barLineArea = Papa.parse(barLineAreaString, {header: true});
+const line22 = Papa.parse(line22String, {header: true});
+const linero22 = Papa.parse(linero22String, {header: true});
