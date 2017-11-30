4b75b04b0cf0da2ad98fb494611be4a204d181a1
Ryan Harings <ryanharings@hotmail.com>
2017-11-30 10:47:19 -0700

    font sizing

--- calculations.js
+++ calculations.js
@@ -125,31 +125,32 @@ $('.primaryUl').on('click', 'li', function() {
   }
   $('.secondaryUl').on('click', '.secondaryLi', function() {
     $('.primaryUl').children().remove();
-    // if (repInfo[$(this).attr('id')]) {
-      // repCard($(this).attr('id'));
-    // } else {
+    if (repInfo[$(this).attr('id')]) {
+      repCard($(this).attr('id'));
+    } else {
       var keys = Object.keys(repInfo);
       keys.forEach((key)=>{
-        // if (repInfo[key].market.includes($(this).attr('id'))) {
-          // repCard(key);
-        // } else if (repInfo[key].manufacturers.includes($(this).attr('id')) || repInfo[key].conglomerate.includes($(this).attr('id'))) {
+        if (repInfo[key].market.includes($(this).attr('id'))) {
+          repCard(key);
+        } else if (repInfo[key].manufacturers.includes($(this).attr('id')) || repInfo[key].conglomerate.includes($(this).attr('id'))) {
         $('.mainMenu').children().removeClass('selected');
-        $('.mfgMain').addClass('selected');
+        // $('.mfgMain').addClass('selected');
           repCard(key);
           $('.header.secondaryLi').text($(this).attr('id'));
           $('.header.secondaryLi:not(:first)').hide();
           $('.header.secondaryLi:first').attr('id',$(this).attr('id'));
           $('.lines.secondaryLi').hide();
-        // }
+        }
       });
-    // }
+    }
   });
 });
 
 function repCard(selectionOutput) {
-  $('.primaryUl').append('<li class="header secondaryLi" id="'+ selectionOutput +'">'+ selectionOutput + ' - '+ repInfo[selectionOutput].rank +'</li>');
-  $('.primaryUl').append('<li class="subHeader secondaryLi" id="'+ repInfo[selectionOutput].market +'">'+ repInfo[selectionOutput].market +'</li>');
-  $('.primaryUl').append('<li class="conglomerate secondaryLi" id="'+ repInfo[selectionOutput].conglomerate +'">Conglomerate: '+ repInfo[selectionOutput].conglomerate +'</li>');
+  // $('.primaryUl').append('<li class="subHeader secondaryLi" id="'+ repInfo[selectionOutput].market +'">'+ repInfo[selectionOutput].market +'</li>');
+  $('.primaryUl').append('<li class="header secondaryLi" id="'+ selectionOutput +'">'+ selectionOutput +'<br><span>Regional Rank #'+ repInfo[selectionOutput].rank +'</span></li>');
+  // $('.primaryUl').append('<li class="subHeader secondaryLi" id="'+ repInfo[selectionOutput].rank +'">Regional Rank #'+ repInfo[selectionOutput].rank +'</li>');
+  $('.primaryUl').append('<li class="conglomerate secondaryLi" id="'+ repInfo[selectionOutput].conglomerate +'">'+ repInfo[selectionOutput].conglomerate +'</li>');
   $('.primaryUl').append('<li class="lines secondaryLi"><ul class="lineCard secondaryUl"></ul></li>');
   repInfo[selectionOutput].manufacturers.forEach(function(manu) {
     $('.lineCard').append('<li class="lines secondaryLi" id="'+ manu +'">'+ manu +'</li>');
